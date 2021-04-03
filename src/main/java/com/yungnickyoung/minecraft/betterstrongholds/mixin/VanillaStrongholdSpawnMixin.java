388c12392a080e70430dc1272885470980a8993b
Nicholas Young <ndy14@ufl.edu>
2021-04-03 19:38:58 -0400

    Vanilla strongholds should no longer spawn.

---
+++ src/main/java/com/yungnickyoung/minecraft/betterstrongholds/mixin/VanillaStrongholdSpawnMixin.java
@@ -0,0 +1,22 @@
+package com.yungnickyoung.minecraft.betterstrongholds.mixin;
+
+import net.minecraft.world.gen.ChunkGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Strongholds are unique structures in that removing them from biomes
+ * is not sufficient to prevent them from spawning. /locate will not work
+ * if they are removed from biomes, but removing them from the noise settings
+ * for the Overworld is also necessary. This mixin is an alternative approach to this
+ * which should be compatible with both regular dimension and JSON dimensions.
+ */
+@Mixin(ChunkGenerator.class)
+public class VanillaStrongholdSpawnMixin {
+    @Inject(method = "func_235958_g_", at = @At(value = "HEAD"), cancellable = true)
+    private void removeVanillaStronghold(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
