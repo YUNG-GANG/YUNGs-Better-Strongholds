c125a36cfefd5343865bad85dc8e372e21c8dc0e
Nicholas Young <ndy14@ufl.edu>
2021-04-04 20:05:09 -0400

    Update generated README.

--- src/main/java/com/yungnickyoung/minecraft/betterstrongholds/init/ModConfig.java
+++ src/main/java/com/yungnickyoung/minecraft/betterstrongholds/init/ModConfig.java
@@ -89,26 +89,34 @@ public class ModConfig {
                 "######################################\n\n" +
                 "  This file contains a BlockSetSelector (see below) describing the probability of a given ore being chosen.\n" +
                 "These probabilities are used in treasure rooms in the stronghold, in which\n" +
-                "piles of ore have a chance of spawning.\n\n" +
-                "  Be VERY SURE the probabilities you provide add up to 1 (i.e., 100%), or else\n" +
-                "you may get unexpected results!\n\n" +
-                "  For information on BlockSetSelectors, see the bottom of this README.\n\n" +
+                "piles of ore have a chance of spawning.\n" +
+                "For information on BlockSetSelectors, see the bottom of this README.\n\n" +
                 "######################################\n" +
                 "#          rareblocks.json           #\n" +
                 "######################################\n\n" +
                 "  This file contains a BlockSetSelector describing the probability of a given block being chosen.\n" +
                 "These probabilities are used in grand libraries, in which\n" +
-                "two rare blocks will spawn.\n\n" +
-                "  Be VERY SURE the probabilities you provide add up to 1 (i.e., 100%), or else\n" +
-                "you may get unexpected results!\n\n" +
-                "  For information on BlockSetSelectors, see the bottom of this README.\n\n" +
+                "two rare blocks will spawn.\n" +
+                "For information on BlockSetSelectors, see the bottom of this README.\n\n" +
+                "######################################\n" +
+                "#          armorstands.json          #\n" +
+                "######################################\n\n" +
+                "  This file contains ItemSetSelectors describing the probability distribution of armor on armor stands.\n" +
+                "Common armor stands spawn in Armoury rooms, while Rare ones are only available in the rare Commander rooms.\n" +
+                "For information on ItemSetSelectors, see the bottom of this README.\n\n" +
+                "######################################\n" +
+                "#          itemframes.json          #\n" +
+                "######################################\n\n" +
+                "  This file contains ItemSetSelectors describing the probability distribution of items in item frames.\n" +
+                "Item frames only spawn in storage rooms and armoury rooms.\n" +
+                "For information on ItemSetSelectors, see the bottom of this README.\n\n" +
                 "######################################\n" +
                 "#         BlockSetSelectors          #\n" +
                 "######################################\n\n" +
-                "BlockSelector: Describes a set of blocks and the probability of each block being chosen.\n" +
-                " - entries: An object where each entry's key is a block, and each value is that block's probability of being chosen.\n" +
+                "Describes a set of blockstates and the probability of each blockstate being chosen.\n" +
+                " - entries: An object where each entry's key is a blockstate, and each value is that blockstate's probability of being chosen.\n" +
                 "      The total sum of all probabilities SHOULD NOT exceed 1.0!\n" +
-                " - defaultBlock: The block used for any leftover probability ranges.\n" +
+                " - defaultBlock: The blockstate used for any leftover probability ranges.\n" +
                 "      For example, if the total sum of all the probabilities of the entries is 0.6, then\n" +
                 "      there is a 0.4 chance of the defaultBlock being selected.\n" +
                 "\n" +
@@ -116,12 +124,18 @@ public class ModConfig {
                 "\"entries\": {\n" +
                 "  \"minecraft:cobblestone\": 0.25,\n" +
                 "  \"minecraft:air\": 0.2,\n" +
-                "  \"minecraft:stonebrick[variant=stonebrick]\": 0.1\n" +
+                "  \"minecraft:stone_bricks\": 0.1\n" +
                 "},\n" +
-                "\"defaultBlock\": \"minecraft:planks[variant=oak]\"\n" +
+                "\"defaultBlock\": \"minecraft:oak_planks\"\n" +
                 "\n" +
                 "For each block, this selector has a 25% chance of returning cobblestone, 20% chance of choosing air,\n" +
-                "10% chance of choosing stone bricks, and a 100 - (25 + 20 + 10) = 45% chance of choosing oak planks (since it's the default block).\n";
+                "10% chance of choosing stone bricks, and a 100 - (25 + 20 + 10) = 45% chance of choosing oak planks (since it's the default block).\n\n" +
+                "######################################\n" +
+                "#         ItemSetSelectors           #\n" +
+                "######################################\n\n" +
+                "Describes a set of items and the probability of each item being chosen.\n" +
+                "Works the same as BlockSetSelectors, but with items instead of blockstates.\n";
+
             try {
                 Files.write(path, readmeText.getBytes());
             } catch (IOException e) {
