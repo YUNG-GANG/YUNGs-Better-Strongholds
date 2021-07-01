d3011377091549acb2029138f0661154b2d047a0
Nicholas Young <ndy14@ufl.edu>
2021-07-01 18:53:00 -0400

    Code cleanup, bugfixes, and dependency resolution.

--- src/main/java/com/yungnickyoung/minecraft/betterstrongholds/config/gui/BSModMenu.java
+++ src/main/java/com/yungnickyoung/minecraft/betterstrongholds/config/gui/BSModMenu.java
@@ -1,8 +1,8 @@
 package com.yungnickyoung.minecraft.betterstrongholds.config.gui;
 
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import com.yungnickyoung.minecraft.betterstrongholds.config.BSConfig;
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
--- src/main/java/com/yungnickyoung/minecraft/betterstrongholds/mixin/VanillaStrongholdSpawnMixin.java
+++ src/main/java/com/yungnickyoung/minecraft/betterstrongholds/mixin/VanillaStrongholdSpawnMixin.java
@@ -1,6 +1,5 @@
 package com.yungnickyoung.minecraft.betterstrongholds.mixin;
 
-import net.minecraft.world.chunk.ChunkManager;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
