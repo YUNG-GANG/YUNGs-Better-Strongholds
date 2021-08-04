f67d4be4ed8ef256419a4872dbd540283aab5e13
Nicholas Young <ndy14@ufl.edu>
2021-08-04 04:52:20 -0400

    v1.1.1. Move dimension whitelist enforcement to be more compatible with Fabric API. Depends on API Fabric-14.

--- src/main/java/com/yungnickyoung/minecraft/betterstrongholds/init/BSModConfiguredStructures.java
+++ src/main/java/com/yungnickyoung/minecraft/betterstrongholds/init/BSModConfiguredStructures.java
@@ -3,18 +3,11 @@ package com.yungnickyoung.minecraft.betterstrongholds.init;
 import com.yungnickyoung.minecraft.betterstrongholds.BetterStrongholds;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.MutableRegistry;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.gen.chunk.StructureConfig;
 import net.minecraft.world.gen.feature.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class BSModConfiguredStructures {
     public static ConfiguredStructureFeature<DefaultFeatureConfig, ? extends StructureFeature<DefaultFeatureConfig>> CONFIGURED_BETTER_STRONGHOLD = BSModStructures.BETTER_STRONGHOLD.configure(FeatureConfig.DEFAULT);
@@ -22,7 +15,6 @@ public class BSModConfiguredStructures {
     public static void init() {
         registerConfiguredStructures();
         addConfiguredStructuresToBiomes();
-        enforceDimensionWhitelist();
     }
 
     /**
@@ -49,22 +41,5 @@ public class BSModConfiguredStructures {
             .add(ModificationPhase.ADDITIONS,
                 biomeSelectionContext -> !BetterStrongholds.blacklistedBiomes.contains(biomeSelectionContext.getBiomeKey().getValue().toString()),
                 modificationContext -> modificationContext.getGenerationSettings().addBuiltInStructure(BSModConfiguredStructures.CONFIGURED_BETTER_STRONGHOLD));
-    }
-
-    /**
-     * Prevent Better Strongholds from spawning in non-whitelisted dimensions.
-     */
-    private static void enforceDimensionWhitelist() {
-        ServerWorldEvents.LOAD.register((MinecraftServer minecraftServer, ServerWorld serverWorld) -> {
-            // Don't spawn in non-whitelisted dimensions
-            if (!BetterStrongholds.whitelistedDimensions.contains(serverWorld.getRegistryKey().getValue().toString())) {
-                // We use a temp map to add our spacing because some mods handle immutable maps
-                Map<StructureFeature<?>, StructureConfig> tempMap = new HashMap<>(serverWorld.getChunkManager().getChunkGenerator().getStructuresConfig().getStructures());
-
-                // Remove Better Stronghold
-                tempMap.keySet().remove(BSModStructures.BETTER_STRONGHOLD);
-                serverWorld.getChunkManager().getChunkGenerator().getStructuresConfig().structures = tempMap;
-            }
-        });
     }
 }
