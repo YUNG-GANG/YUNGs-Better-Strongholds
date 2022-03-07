03706e88d90a4a04bb2bb89e57417cd25804df64
Nick Young <yungnickyoung@gmail.com>
2022-03-07 17:40:05 -0500

    Fix eyes not locating Better Strongholds. Fix eyes still trying to locate vanilla strongholds. Included in v1.1.0.

--- src/main/java/com/yungnickyoung/minecraft/betterstrongholds/mixin/DisableVanillaStrongholdsMixin.java
+++ src/main/java/com/yungnickyoung/minecraft/betterstrongholds/mixin/DisableVanillaStrongholdsMixin.java
@@ -10,11 +10,14 @@ import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/**
+ * Stops vanilla strongholds from generating by preventing their structure starts
+ * from being generated.
+ */
 @Mixin(ChunkGenerator.class)
 public class DisableVanillaStrongholdsMixin {
     @Inject(method = "tryGenerateStructure", at = @At(value = "HEAD"), cancellable = true)
