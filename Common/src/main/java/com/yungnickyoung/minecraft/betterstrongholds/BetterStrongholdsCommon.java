299479034b2e5356f38f47ec083dfa1ef15cc28e
williambl <william@williambl.com>
2026-09-10 14:01:51 +0100

    Switch to using new YUNG's API LocateReplacer API
    YUNG-GANG/YUNGs-Better-Desert-Temples#51

--- Common/src/main/java/com/yungnickyoung/minecraft/betterstrongholds/BetterStrongholdsCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterstrongholds/BetterStrongholdsCommon.java
@@ -3,6 +3,11 @@ package com.yungnickyoung.minecraft.betterstrongholds;
 import com.yungnickyoung.minecraft.betterstrongholds.module.ConfigModule;
 import com.yungnickyoung.minecraft.betterstrongholds.services.Services;
 import com.yungnickyoung.minecraft.yungsapi.api.YungAutoRegister;
+import com.yungnickyoung.minecraft.yungsapi.api.world.structure.locate.LocateReplacer;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,5 +19,9 @@ public class BetterStrongholdsCommon {
     public static void init() {
         YungAutoRegister.scanPackageForAnnotations("com.yungnickyoung.minecraft.betterstrongholds.module");
         Services.MODULES.loadModules();
+
+        LocateReplacer.register(BuiltinStructures.STRONGHOLD,
+                ResourceKey.create(Registries.STRUCTURE, Identifier.fromNamespaceAndPath(MOD_ID, "stronghold")),
+                () -> true);
     }
 }
